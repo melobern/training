42f691fdcedd7c0d59445fb1ce904bdb1066bd23
Mélodie <melodie.bernard123@yahoo.fr>
2023-09-07 18:20:44 +0200

    change in progress...

--- social_security_num/social_num.c
+++ social_security_num/social_num.c
@@ -6,7 +6,7 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 23:16:14 by mbernard          #+#    #+#             */
-/*   Updated: 2023/09/07 17:06:39 by mbernard         ###   ########.fr       */
+/*   Updated: 2023/09/07 18:20:30 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,43 @@ void	print_just_key(char *str)
 	long long	key;
 
 	key = calcul_the_key(str);
+	if (str[5] == '1' && (str[6] == '9' || str[6] == '8'))
+	{
+		ft_putstr("Corsican !\n");
+		str[5] = '2';
+		if (str[6] == '9')
+			str[6] = 'A';
+		else
+			str[6] = 'B';
+	}
 	ft_putstr("The key of ");
 	ft_putstr(str);
 	ft_putstr(" is : ");
 	ft_putnbr(key);
 }
 
+void	one_arg_num(char **av)
+{
+	if (ft_strlen(av[1]) < 13)
+		ft_putstr("Oups ! This number is way too short !");
+	else if (ft_strlen(av[1]) == 13)
+		print_just_key(av[1]);
+	else if (ft_strlen(av[1]) == 14)
+		ft_putstr("Oups ! Not enough or too many numbers !");
+	else if (ft_strlen(av[1]) == 15)
+		calcul_nums(av[1]);
+	else if (ft_strlen(av[1]) > 15)
+		ft_putstr("Oups ! This number is way too long !");
+}
+
 int	main(int ac, char **av)
 {
-	if (ac > 1)
+	if (ac == 2)
+		one_arg_num(av);
+	else if (ac > 2 && ac < 14)
 	{
-		if (ft_strlen(av[1]) < 13)
-			ft_putstr("Oups ! This number is way too short !");
-		else if (ft_strlen(av[1]) == 13)
-			print_just_key(av[1]);
-		else if (ft_strlen(av[1]) == 14)
-			ft_putstr("Oups ! Not enough or too many numbers !");
-		else if (ft_strlen(av[1]) == 15)
-		{
-			calcul_nums(av[1]);
-		}
-		else if (ft_strlen(av[1]) > 15)
-			ft_putstr("Oups ! This number is way too long !");
+		convert(ac, av);
+		one_arg_num[av[1]]
 	}
 	ft_putchar('\n');
 	return (0);
